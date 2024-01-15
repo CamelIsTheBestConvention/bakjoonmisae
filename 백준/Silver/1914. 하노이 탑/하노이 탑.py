47fc77e23e4403004376fed7145142d94ce58aef
SungJunMoon <115637701+CamelIsTheBestConvention@users.noreply.github.com>
2024-01-15 17:05:11 +0900

    [Silver I] Title: 하노이 탑, Time: 512 ms, Memory: 127572 KB -BaekjoonHub

---
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.py
@@ -0,0 +1,13 @@
+def hanoi(n, start, sub, finish):
+    if n == 1:
+        print(start,finish)
+    else:
+        hanoi(n-1, start, finish, sub)
+        print(start, finish)
+        hanoi(n-1, sub, start, finish)
+
+n = int(input())
+
+print(2**n-1)
+if n<=20:
+    hanoi(n, '1', '2', '3')
