9b0f29685ef3ab9bf97eeb33fbdf3974214e1c01
SungJunMoon <115637701+CamelIsTheBestConvention@users.noreply.github.com>
2024-01-23 23:00:17 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1548 ms, Memory: 162764 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+def fine_city(line, x, visited, dist):
+    queue = deque([x])
+    visited[x] = True
+
+    while queue:
+        pop = queue.popleft()
+
+        for i in line[pop]:
+            if not visited[i]:
+                queue.append(i)
+                dist[i] = dist[pop] + 1
+                visited[i] = True
+
+    found = False
+    for s in range(len(dist)):
+        if dist[s] == k:
+            print(s)
+            found = True
+
+    if not found:
+        print(-1)
+
+n, m, k, x = list(map(int, input().split()))
+
+line = [[] for _ in range(n+1)]
+
+visited = [False] * (n+1)
+
+for i in range(m):
+    a, b = list(map(int, input().split()))
+    line[a].append(b)
+
+dist = [0] * (n+1)
+
+fine_city(line, x, visited, dist)
