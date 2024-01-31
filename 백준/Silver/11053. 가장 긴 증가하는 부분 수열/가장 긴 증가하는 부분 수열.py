2bfb186d5a6bfcae75e049edca07d0046fa79643
SungJunMoon <115637701+CamelIsTheBestConvention@users.noreply.github.com>
2024-01-31 16:09:20 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -2,20 +2,14 @@ import sys
 input = sys.stdin.readline
 
 def longLCS(a):
-    # 받은 문자열의 길이를 체크
     length = len(a)
-    # 처음 시작을 1로 해야 원소가 처음 나올때 1이 체크된다.
-    list = [1]*length
+    list = [1]*n
 
-    # 처음껀 비교대상이 없기 때문에 1부터 시작
     for i in range(1, length):
-        # i 이전 수들끼리 비교해야 하기 때문에 i까지 반복
         for j in range(i):
-            # i값이 j값보다 크고(증가 수열) 리스트에 i를 넣었을 때 기존 리스트보다 길면 갱신
             if a[i] > a[j] and list[i] < list[j]+1:
                 list[i] = list[j]+1
 
-    # 리스트에서 제일 최대값을 반환
     return max(list)
 
 n = int(input())
