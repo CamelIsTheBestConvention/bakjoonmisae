22490659c6d85a36c323002e07d95cbce1d4440e
SungJunMoon <115637701+CamelIsTheBestConvention@users.noreply.github.com>
2024-02-20 20:47:35 +0900

    [Silver V] Title: 야구 시즌, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/21760. 야구 시즌/야구 시즌.py
+++ 백준/Silver/21760. 야구 시즌/야구 시즌.py
@@ -1,29 +1,10 @@
 import sys
 input = sys.stdin.readline
 
-def Baseball_season():
-    case = int(input())
-
-    for _ in range(case):
-        # 지역리그 n, 팀 m, 지역 리그와 타지역 리그의 비율 k, 전체 경기수 d
-        n, m, k, d = list(map(int, input().split()))
-        b = 1
-
-        while True:
-            a = b * k
-            # 같은 지역리그 경기 수+ 다른 지역리그 경기 수
-            total_game = ((m*(m-1)//2) * a * n) + (b * m * (m*(n-1)))
-
-            if total_game > d:
-                if b == 1:
-                    print(-1)
-                else:
-                    b -= 1
-                    a = b * k
-                    total_game = ((m*(m-1)//2) * a * n) + (b * m * (m*(n-1)))
-                    print(total_game)
-
-                break
-            b += 1
-
-Baseball_season()
+for _ in range(int(input())):
+    n, m, k, d = map(int, input().split())
+    b = 2*d//(k*n*m*(m-1) + m*m*n*(n-1))
+    if b:
+        print(m*(m-1)*n*k*b//2 + m*m*n*(n-1)*b//2)
+    else:
+        print(-1)
