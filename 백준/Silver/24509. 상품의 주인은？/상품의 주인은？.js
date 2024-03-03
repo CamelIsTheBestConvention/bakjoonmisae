a87ad51f99053f3b8a2b45c9ee995747ebc11f12
SungJunMoon <115637701+CamelIsTheBestConvention@users.noreply.github.com>
2024-03-03 18:00:26 +0900

    [Silver V] Title: 상품의 주인은?, Time: 1384 ms, Memory: 95344 KB -BaekjoonHub

---
+++ 백준/Silver/24509. 상품의 주인은？/상품의 주인은？.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+const itemMaster = (N) => {
+    const studentScore = [];
+    const item_master = [];
+
+    for(let i = 1; i <= N; i++) {
+        let score = input[i].split(" ").map(Number);
+        studentScore.push(score);
+    }
+
+    // 과목별로 정렬
+    for(let j = 1; j <= 4; j++) {
+        studentScore.sort((a, b) => {
+            if (b[j] !== a[j]) {
+                return b[j] - a[j];
+            } else {
+                return a[0] - b[0];
+            }
+        });
+
+        // 상품을 아직 받지 않은 학생 중에서 점수가 가장 높은 학생을 찾음
+        for(let k = 0; k < N; k++) {
+            if (!item_master.includes(studentScore[k][0])) {
+                item_master.push(studentScore[k][0]);
+                break;
+            }
+        }
+    }
+
+    console.log(item_master.join(' '));
+}
+
+let N = Number(input[0]);
+
+itemMaster(N);
