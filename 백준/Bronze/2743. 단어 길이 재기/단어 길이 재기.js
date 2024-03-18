9555839bd5ecd72ecc8ad15609cc923f873624e7
SungJunMoon <115637701+CamelIsTheBestConvention@users.noreply.github.com>
2024-03-18 10:19:45 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 128 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+const wordLen = input[0].length;
+
+console.log(wordLen);
