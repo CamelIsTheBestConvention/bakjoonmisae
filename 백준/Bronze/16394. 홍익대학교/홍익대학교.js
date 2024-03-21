3650559caebf1ae1965023713e01310d7e37203c
SungJunMoon <115637701+CamelIsTheBestConvention@users.noreply.github.com>
2024-03-21 12:53:35 +0900

    [Bronze V] Title: 홍익대학교, Time: 124 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+const n = Number(input[0]);
+
+console.log(n - 1946);
