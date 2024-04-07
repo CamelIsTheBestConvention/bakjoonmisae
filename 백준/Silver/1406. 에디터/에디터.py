a0e1234c1f71030860d830684116fa12ce429c1c
SungJunMoon <115637701+CamelIsTheBestConvention@users.noreply.github.com>
2024-04-07 21:56:09 +0900

    [Silver II] Title: 에디터, Time: 288 ms, Memory: 41540 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -0,0 +1,20 @@
+import sys
+
+left_stack = list(input().strip())
+right_stack = []
+n = int(input())
+
+for _ in range(n):
+    command = sys.stdin.readline().strip().split()
+    
+    if command[0] == 'L' and left_stack:
+        right_stack.append(left_stack.pop())
+    elif command[0] == 'D' and right_stack:
+        left_stack.append(right_stack.pop())
+    elif command[0] == 'B' and left_stack:
+        left_stack.pop()
+    elif command[0] == 'P':
+        left_stack.append(command[1])
+
+result = ''.join(left_stack + right_stack[::-1])
+print(result)
