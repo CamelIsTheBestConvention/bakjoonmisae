e759c95f50b9161df6efb2d2c0f19ac64cba8b3f
문성준 <115637701+CamelIsTheBestConvention@users.noreply.github.com>
2024-07-12 15:23:11 +0900

    [Silver I] Title: 완전 이진 트리, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.py
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.py
@@ -1,26 +1,22 @@
-def build_tree(nodes, depth, levels, current_level=0):
-    if not nodes:
-        return
-    
-    mid = len(nodes) // 2
-    levels[current_level].append(nodes[mid])
-    
-    build_tree(nodes[:mid], depth, levels, current_level + 1)
-    build_tree(nodes[mid + 1:], depth, levels, current_level + 1)
+import sys
+input = sys.stdin.read
 
-def main():
-    import sys
-    input = sys.stdin.read
-    data = input().split()
-    
-    K = int(data[0])
-    nodes = list(map(int, data[1:]))
-    
-    levels = [[] for _ in range(K)]
-    build_tree(nodes, K, levels)
-    
-    for level in levels:
-        print(' '.join(map(str, level)))
+input_data = input().split()
+K = int(input_data[0])
+arr = list(map(int, input_data[1:]))
+levels = [[] for _ in range(K)]
+stack = [(0, len(arr) - 1, 0)]
 
-if __name__ == "__main__":
-    main()
+while stack:
+    start, end, level = stack.pop()
+    if start > end:
+        continue
+    
+    mid = (start + end) // 2
+    levels[level].append(arr[mid])
+    
+    stack.append((mid + 1, end, level + 1))
+    stack.append((start, mid - 1, level + 1))
+
+for level in levels:
+    print(' '.join(map(str, level)))
