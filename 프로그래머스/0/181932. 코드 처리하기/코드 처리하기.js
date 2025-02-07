32dcc7a68e93e90223555067f9337c4efb3f5e82
문성준 <115637701+CamelIsTheBestConvention@users.noreply.github.com>
2025-02-07 23:44:48 +0900

    [level 0] Title: 코드 처리하기, Time: 9.80 ms, Memory: 39.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,14 @@
+function solution(code) {
+    var answer = '';
+    var mode = 0;
+   
+    for(let i=0; i<code.length; i++){
+       if (mode == 0){
+           code[i] == "1" ? (mode = 1 ) : (i % 2 == 0 && (answer += code[i]));
+       }else{
+           code[i] == "1" ? (mode = 0) : (i % 2 !== 0 && (answer += code[i]));
+       }
+    }
+    
+    return answer.length>0 ? answer : "EMPTY";
+}
