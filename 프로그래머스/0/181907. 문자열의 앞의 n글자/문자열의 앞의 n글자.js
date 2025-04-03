b3e5efd34355eebb5733fde9e2f1c0736fe869c5
문성준 <115637701+CamelIsTheBestConvention@users.noreply.github.com>
2025-04-03 23:59:41 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.js
@@ -0,0 +1,9 @@
+function solution(my_string, n) {
+    var answer = '';
+    let arr = my_string.split('');
+    
+    for (let i = 0; i < n; i++) {
+        answer += arr[i]
+    }
+    return answer;
+}
